0a334acda4f31e5b80b3957776da614b75e282f1
Jeremy Scheff <jdscheff@gmail.com>
2016-02-05 20:05:58 -0500

    More concise definition of KeyRange functions

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ class Backboard {
             const latestSchema = schemas[schemas.length - 1];
 
             const request = indexedDB.open(name, latestSchema.version);
-            request.onerror = (event) => reject(event.target.error);
+            request.onerror = event => reject(event.target.error);
             request.onblocked = () => reject(new Error('Unexpected blocked event'));
-            request.onupgradeneeded = (event) => upgrade(event, schemas);
-            request.onsuccess = (event) => resolve(new DB(event.target.result));
+            request.onupgradeneeded = event => upgrade(event, schemas);
+            request.onsuccess = event => resolve(new DB(event.target.result));
         });
     }
 
     static delete(name) {
         return new Backboard.Promise((resolve, reject) => {
             const request = indexedDB.deleteDatabase(name);
-            request.onerror = (event) => reject(event.target.error);
+            request.onerror = event => reject(event.target.error);
             request.onblocked = () => resolve(); // http://stackoverflow.com/a/27871590/786644
             request.onupgradeneeded = () => reject(new Error('Unexpected upgradeneeded event'));
             request.onsuccess = () => resolve();
@@ -27,20 +27,12 @@ class Backboard {
     static setPromiseConstructor(PromiseConstructor) {
         Backboard.Promise = PromiseConstructor;
     }
-
-    static lowerBound() {
-        return IDBKeyRange.lowerBound.apply(IDBKeyRange, arguments);
-    }
-    static upperBound() {
-        return IDBKeyRange.upperBound.apply(IDBKeyRange, arguments);
-    }
-    static only() {
-        return IDBKeyRange.only.apply(IDBKeyRange, arguments);
-    }
-    static bound() {
-        return IDBKeyRange.bound.apply(IDBKeyRange, arguments);
-    }
 }
+
+['lowerBound', 'upperBound', 'only', 'bound'].forEach((keyRangeFunction) => {
+    Backboard[keyRangeFunction] = (...args) => IDBKeyRange.lowerBound.apply(IDBKeyRange, args);
+});
+
 Backboard.Promise = Promise;
 
 module.exports = Backboard;
