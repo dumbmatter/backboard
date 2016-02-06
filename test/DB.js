b889d94df9811724be7172cd6b5d9d978c41a871
Jeremy Scheff <jdscheff@gmail.com>
2016-02-05 19:24:20 -0500

    DB even listeners, although they are not yet used for anything

--- test/DB.js
+++ test/DB.js
@@ -34,6 +34,43 @@ describe('DB', () => {
         return Backboard.delete('test');
     });
 
+    describe('events', () => {
+        it('on and off', (done) => {
+            let quotaexceededCount = 0;
+
+            db.on('quotaexceeded', value => {
+                assert.equal(value, 'bar');
+                quotaexceededCount += 1;
+                if (quotaexceededCount === 2) {
+                    done();
+                }
+            });
+            db.on('quotaexceeded', value => {
+                assert.equal(value, 'bar');
+                quotaexceededCount += 1;
+                if (quotaexceededCount === 2) {
+                    done();
+                }
+            });
+            db.on('versionchange', () => {
+                done(new Error('Unexpected versionchange event'));
+            });
+
+            const toRemove = () => {
+                quotaexceededCount += 1;
+                done(new Error('Removed listener was called'));
+            };
+            db.on('quotaexceeded', toRemove);
+            db.off('quotaexceeded', toRemove);
+
+            db._emit('quotaexceeded', 'bar');
+        });
+
+        it('error on invalid listener name', () => {
+            assert.throws(() => db.on('foo', () => {}), /Invalid listener name "foo"/);
+        });
+    });
+
     describe('properties', () => {
         it('name', () => {
             assert.equal(db.name, 'test');
